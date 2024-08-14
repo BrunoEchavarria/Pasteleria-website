4eeeae614b716aa6d1343d4184267ed0e3330a82
Bruno Echavarria <brunoechavarria18@gmail.com>
2024-08-14 20:00:03 -0300

    Upload project

---
+++ resources/views/components/hero.blade.php
@@ -0,0 +1,11 @@
+<div class="container hero d-flex flex-column-reverse flex-md-row justify-content-around pt-4 py-md-5 px-md-0">
+    <div class="text-content col-md-5 d-flex flex-column justify-content-md-center align-items-center align-items-md-start">
+        <h1 class="title-hero">
+            Nos damos el gusto de producir lo que mas te gusta!
+        </h1>
+        <a href="#">Ver productos!</a>
+    </div>
+    <div class="img-content col-md-6 col-lg-5"> 
+        <img src="/img/pastel.jpg" alt="Imagen ilustrativa de un pastel">
+    </div>
+</div>
---
+++ resources/views/components/products.blade.php
@@ -0,0 +1,27 @@
+<div class="container products">
+    <div class="d-sm-flex justify-content-around">
+        <h2 class="col-lg-6 m-auto products-title py-5 d-flex col-md-12">
+            Comprá tus productos favoritos
+        </h2>
+        <div class=" d-none d-sm-flex col-lg-5">
+            
+        </div>
+    </div>
+    <div class="container row d-md-flex justify-content-md-around">
+        <div class="products-card col-12 col-sm-5 col-md-3">
+                <img src="/img/pastel.jpg" alt="">
+                <p>Esto es una descripcion del producto 1</p>
+                <a href="#">Ver productos!</a>
+        </div>
+        <div class="products-card col-12 col-sm-5 col-md-3">
+                <img src="/img/pastel.jpg" alt="">
+                <p>Esto es una descripcion</p>
+                <a href="#">Ver productos!</a>
+        </div>
+        <div class="products-card col-12 col-sm-5 col-md-3">
+                <img src="/img/pastel.jpg" alt="">
+                <p>Esto es una descripcion</p>
+                <a href="#">Ver productos!</a>
+        </div>
+    </div>
+</div>
